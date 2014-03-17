3710367bbf98253499b25ff37e51e420f31d8617
jockschrader <jk.schrader@computer.org>
2014-03-17 19:32:10 +0100

    test

---
+++ Schildt_Uebungsbereich/src/JKSJavaTestPack/JKSJavaTest.java
@@ -0,0 +1,13 @@
+package JKSJavaTestPack;
+
+public class JKSJavaTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Wurstfabrik 123.");
+	}
+
+}
